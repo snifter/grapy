507c5dee7346192c3f3c9b61a1ac3d4776643141
Marek Podsiadły <marekpodsiadly@gmail.com>
2018-04-30 00:02:15 +0200

    NodeStore created

---
+++ grapy/store/node.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+from contextlib import AbstractContextManager
+from os import path, os
+from struct import Struct
+
+# little-endian
+# 1 byte - in use - deleted or not (bool)
+# 8 bytes - first relationship pointer (integer)
+# 8 bytes - first property pointer (integer)
+# 4 * 1 byte - labels pointers (integer)
+from threading import Lock
+
+NODE_RECORD_FORMAT = '<?QQBBBB'
+
+NODE_STORE_FILE_NAME = 'grapy.nodes.db'
+
+NodeRecord = namedtuple('NodeRecord', 'in_use first_relationship first_property label_1 label_2 label_3 label_4')
+
+
+class NodeStore(AbstractContextManager):
+    def __init__(self, dir='.'):
+        self.__struct = Struct(NODE_RECORD_FORMAT)
+        self.__file = None
+        self.__dir = dir
+        self.__lock = Lock()
+
+    @property
+    def record_size(self):
+        return self.__struct.size
+
+    @property
+    def store_file(self):
+        return path.join(self.__dir, NODE_STORE_FILE_NAME)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def open(self):
+        self.__file = open(self.store_file, 'wb')
+
+    def close(self):
+        if not self.__file:
+            return
+
+        self.__file.close()
+
+    def write(self, record):
+        with self.__lock:
+            self.__file.seek(0, os.SEEK_END)
+            record_id = self.__file.tell()
+
+            self.__file.write(self.__struct.pack(record))
+
+        return record_id
+
+    def read(self, record_id):
+        with self.__lock:
+            self.__file.seek(record_id, os.SEEK_SET)
+
+            buffer = self.__file.read(self.record_size)
+            record = NodeRecord._make(self.__struct.unpack(buffer))
+
+        return record
