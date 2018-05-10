667391a8888ad4bb23e2bb2ceb0a0628c90109e6
Marek Podsiadły <marekpodsiadly@gmail.com>
2018-05-10 21:40:52 +0200

    property row header

--- grapy/store/property.py
+++ grapy/store/property.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+from enum import IntEnum
+
 from grapy.store.base.descryptor import ConstantLengthBytes
 from grapy.store.base.record import Record, RecordFactory
 from grapy.store.base.store import RecordStore
@@ -7,7 +10,26 @@ PROPERTY_NAME_RECORD_FORMAT = '<40s'
 # little-endian
 # 40 bytes - name - char[]
 
+PROPERTY_HEADER_RECORD_FORMAT = '<QQHQ'
+# little-endian
+# property name pointer (integer)
+# next property pointer (integer)
+# property type (integer)
+# property length (integer)
+# property value is saved after header
+
 PROPERTY_NAME_STORE_FILE_NAME = 'grapy.propertynames.db'
+PROPERTY_STORE_FILE_NAME = 'grapy.properties.db'
+
+
+PropertyHeader = namedtuple('PropertyHeader', 'name_pointer next_property type length')
+
+
+class PropertyType(IntEnum):
+    INTEGER = 1
+    FLOAT = 2
+    BOOL = 3
+    BYTES = 4
 
 
 class PropertyNameRecord(Record):
